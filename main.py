2a875d1426672ea2c651cb5f5ca67b93193b5773
Dev Thakkar <devthakkar@usf.edu>
2024-05-07 18:52:54 -0400

    Add open Oasis functionality

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
+
+
 
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+
+
+driver.get("https://my.usf.edu/myusf/home_myusf/index")
+time.sleep(5)
+
+
+elem = driver.find_element(By.ID, "i0116")
+elem.send_keys("devthakkar@usf.edu")
+time.sleep(5)
+
+next_button = driver.find_element(By.ID, "idSIButton9")
+next_button.click()
+time.sleep(5)
+
+elem = driver.find_element(By.ID, "i0118")
+elem.send_keys("Dnrnt*5475%$&%")
+time.sleep(5)
+
+sign_in_button = driver.find_element(By.ID, "idSIButton9")
+sign_in_button.click()
+time.sleep(5)
+
+value = driver.find_element(By.ID, "idRichContext_DisplaySign")
+print(value.text)
+time.sleep(30)
+
+yes_button = driver.find_element(By.ID, "idSIButton9")
+yes_button.click()
+time.sleep(5)
+
+links = driver.find_elements(By.CLASS_NAME, "kgo-publish-tile-action")
+
+# Iterate through the links
+for link in links:
+    # Check if the link's href attribute matches the desired URL
+    if link.get_attribute("href") == "https://bannersso.usf.edu/ssomanager/c/SSB":
+        # Click the link
+        link.click()
+        break
+time.sleep(20)
+
 driver.close()
